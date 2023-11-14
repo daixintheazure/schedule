c42aae67f7138501755ed9d15f8ea8f88a2291c0
Mario Dickerson <dai.dickerson@gmail.com>
2023-11-13 18:49:55 -0600

    created employee and workstation

---
+++ src/main/java/com/daixin/schedule/models/Employee.java
@@ -0,0 +1,61 @@
+package com.daixin.schedule.models;
+
+import jakarta.persistence.GeneratedValue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Employee {
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int hours;
+
+    private final List<WorkStation> workStation = new ArrayList<>();
+
+    //Constructors
+
+    public Employee() {}
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    //Hash mapping
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+}
---
+++ src/main/java/com/daixin/schedule/models/WorkStation.java
@@ -0,0 +1,55 @@
+package com.daixin.schedule.models;
+
+import jakarta.persistence.GeneratedValue;
+
+import java.util.Objects;
+
+public class WorkStation {
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String shortHand;
+
+    // Constructor
+    public WorkStation () {}
+
+    public WorkStation(String name, String shortHand) {
+        this.name = name;
+        this.shortHand = shortHand;
+    }
+
+    // Hash
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WorkStation that = (WorkStation) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortHand() {
+        return shortHand;
+    }
+
+    public void setShortHand(String shortHand) {
+        this.shortHand = shortHand;
+    }
+}
